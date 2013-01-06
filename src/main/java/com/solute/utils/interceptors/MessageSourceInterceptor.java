a968b17fd27ad48eb57ac9e991c8b4c2f858d4a9
jcooky <bak723@gmail.com>
2013-01-06 11:56:40 +0900

    Signed-off-by: jcooky <bak723@gmail.com>

---
+++ src/main/java/com/solute/utils/interceptors/MessageSourceInterceptor.java
@@ -0,0 +1,7 @@
+package com.solute.utils.interceptors;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class MessageSourceInterceptor extends HandlerInterceptorAdapter {
+
+}
