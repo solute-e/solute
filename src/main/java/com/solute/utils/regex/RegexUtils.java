bbeff8588e6470f3a2e777c99cf112b0d97abdb8
jcooky <bak723@gmail.com>
2013-03-07 16:02:53 +0900

    쓸데없는거 삭

--- src/main/java/com/solute/utils/regex/RegexUtils.java
+++
@@ -1,9 +0,0 @@
-package com.solute.utils.regex;
-
-public class RegexUtils {
-	public static final String EMAIL_FORMAT = "([a-z]|[0-9])+@([a-z]|[0-9])+\\.([a-z]|[0-9])+";
-	
-	public static boolean matchesEmail(String email) {
-		return email.matches(EMAIL_FORMAT);
-	}
-}
